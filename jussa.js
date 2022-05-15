50c27b26312105103cf2f87ae0ac754a393ccb90
Muhiddin <muhiddin@MacBook-Air-Muhiddin.local>
2022-05-16 02:56:37 +0500

    ikki yonga ochiladigan qilindi

--- jussa.js
+++ jussa.js
@@ -1,10 +1,16 @@
 let header = document.getElementById("header");
+let header2 = document.getElementById("header2");
 let wardrobe = document.getElementById("wardrobe");
 let header__teext1 = document.getElementById("header__teext1");
 
 window.addEventListener("scroll", () => {
   let value = window.scrollY;
-  wardrobe.style.top = -value * 1 + "px";
-  header__teext1.style.opacity= "100"-value+"%"
-  header
+  // wardrobe.style.top = -value * 1 + "px";
+  header__teext1.style.opacity = "100" - value + "%";
+  header.style.left = -value * 1 + "px";
+  header2.style.right = -value * 1 + "px";
+});
+header__teext1.addEventListener("click", () => {
+  header.style.position = "absolute";
+  header.style.right = "0";
 });
